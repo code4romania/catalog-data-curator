baac9f09ccdf06aaa0b1d130c92c3f00f83ec88d
hmgheorghe <mirceahalmagean@gmail.com>
2017-04-29 17:31:23 +0300

    fixed swagger ui

--- src/main/java/ro/code4/curator/config/MvcConfig.java
+++ src/main/java/ro/code4/curator/config/MvcConfig.java
@@ -19,8 +19,8 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath*:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars*/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
